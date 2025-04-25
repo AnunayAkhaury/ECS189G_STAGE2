1120e359151005a71e3665e4a945f2391ca6d7a7
VishReddy <vishwareddy@gmail.com>
2025-04-25 16:36:19 -0700

    metrics

--- local_code/stage_2_code/Evaluate_Accuracy.py
+++ local_code/stage_2_code/Evaluate_Accuracy.py
@@ -6,7 +6,7 @@ Concrete Evaluate class for a specific evaluation metrics
 # License: TBD
 
 from local_code.base_class.evaluate import evaluate
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 class Evaluate_Accuracy(evaluate):
@@ -14,5 +14,43 @@ class Evaluate_Accuracy(evaluate):
     
     def evaluate(self):
         print('evaluating performance...')
-        return accuracy_score(self.data['true_y'], self.data['pred_y'])
+        true_y = self.data['true_y']
+        pred_y = self.data['pred_y']
+        
+        # Calculate all metrics
+        accuracy = accuracy_score(true_y, pred_y)
+        
+        # Weighted metrics (accounts for class imbalance)
+        weighted_precision = precision_score(true_y, pred_y, average='weighted')
+        weighted_recall = recall_score(true_y, pred_y, average='weighted')
+        weighted_f1 = f1_score(true_y, pred_y, average='weighted')
+        
+        # Macro metrics (arithmetic mean across classes)
+        macro_precision = precision_score(true_y, pred_y, average='macro')
+        macro_recall = recall_score(true_y, pred_y, average='macro')
+        macro_f1 = f1_score(true_y, pred_y, average='macro')
+        
+        # Micro metrics (aggregate TP, FP, FN across all classes)
+        micro_precision = precision_score(true_y, pred_y, average='micro')
+        micro_recall = recall_score(true_y, pred_y, average='micro')
+        micro_f1 = f1_score(true_y, pred_y, average='micro')
+        
+        return {
+            'accuracy': accuracy,
+            'weighted': {
+                'precision': weighted_precision,
+                'recall': weighted_recall,
+                'f1': weighted_f1
+            },
+            'macro': {
+                'precision': macro_precision,
+                'recall': macro_recall,
+                'f1': macro_f1
+            },
+            'micro': {
+                'precision': micro_precision,
+                'recall': micro_recall,
+                'f1': micro_f1
+            }
+        }
         
