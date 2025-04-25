3f851334b3abd3d3ed17aed02e17b70602dea8f8
VishReddy <vishwareddy@gmail.com>
2025-04-25 16:51:54 -0700

    metrics pt2

--- local_code/stage_2_code/Method_MLP.py
+++ local_code/stage_2_code/Method_MLP.py
@@ -90,14 +90,14 @@ class Method_MLP(method, nn.Module):
 
             if epoch%100 == 0:
                 accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+                # print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
                 metrics = accuracy_evaluator.evaluate()
                 print(f'Epoch: {epoch}')
-                print(f"Loss: {train_loss.item():.4f}")
-                print(f"Accuracy: {metrics['accuracy']:.4f}")
-                print(f"Weighted F1: {metrics['weighted']['f1']:.4f}")
-                print(f"Macro F1: {metrics['macro']['f1']:.4f}")
-                print(f"Micro F1: {metrics['micro']['f1']:.4f}")
+                print(f"Loss: {train_loss.item()}")
+                print(f"Accuracy: {metrics['accuracy']}")
+                print(f"Weighted F1: {metrics['weighted']['f1']}")
+                print(f"Micro F1: {metrics['micro']['f1']}")
+                print(f"Macro F1: {metrics['macro']['f1']}")
                 print('-------------------')
     
     def test(self, X):
