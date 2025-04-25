10d6a5a40306748b5846ca21558fff3b429bcc9c
Rohan Sheth <rohanjaminsheth@gmail.com>
2025-04-25 16:42:29 -0700

    mlp

--- local_code/stage_2_code/Method_MLP.py
+++ local_code/stage_2_code/Method_MLP.py
@@ -30,7 +30,7 @@ class Method_MLP(method, nn.Module):
         # check here for nn.ReLU doc: https://pytorch.org/docs/stable/generated/torch.nn.ReLU.html
         self.activation_func_1 = nn.ReLU()
         self.fc_layer_2 = nn.Linear(20, 20)
-        self.fc_layer_3 = nn.Linear(20, 20)
+        self.fc_layer_3 = nn.Linear(20, 10)
         # check here for nn.Softmax doc: https://pytorch.org/docs/stable/generated/torch.nn.Softmax.html
         self.activation_func_2 = nn.Softmax(dim=1)
         # Add loss history tracking
