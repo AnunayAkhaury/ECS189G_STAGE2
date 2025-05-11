17d367ea20b3f75275ae69d0e669f5ce32841831
Rohan Sheth <rohanjaminsheth@gmail.com>
2025-05-10 19:33:25 -0700

    MINST Done

--- script/stage_3_code/script_mlp_train.py
+++ script/stage_3_code/script_mlp_train.py
@@ -1,5 +1,10 @@
 import pickle
 from matplotlib import pyplot as plt
+from local_code.stage_3_code.CNN_MNIST import CNN_MNIST
+from local_code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
 f = open('../../data/stage_3_data/MNIST', 'rb')
 data = pickle.load(f)
@@ -15,5 +20,37 @@ train_image = [instance['image'] for instance in data['train']]
 test_image = [instance['image'] for instance in data['test']]
 
 
+mlp = CNN_MNIST('MLP', 'Multi-Layer Perceptron for MNIST Classification')
 
+mlp.data = {
+    'train': {
+        'X': train_image,
+        'y': train_labels
+    },
+    'test': {
+        'X': test_image,
+        'y': test_labels
+    }
+}
 
+evaluate_obj = Evaluate_Accuracy('accuracy', '')
+
+print('************ Start ************')
+print('Training MLP...')
+result = mlp.run()
+
+loss_history = mlp.loss_history
+
+plt.figure(figsize=(10, 6))
+plt.plot(loss_history, label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('MLP Training Convergence')
+plt.grid(True)
+plt.legend()
+
+#plt.savefig('../../result/stage_2_result/training_convergence.png')
+#plt.close()
+
+evaluate_obj.data = result
+metrics = evaluate_obj.evaluate()
