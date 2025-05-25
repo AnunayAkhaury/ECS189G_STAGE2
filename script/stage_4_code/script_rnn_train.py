4c571f7a394c95a3220bb3e4411d74b05afb5a4c
Rohan Sheth <rohanjaminsheth@gmail.com>
2025-05-25 13:26:15 -0700

    revert

--- script/stage_4_code/script_rnn_train.py
+++ script/stage_4_code/script_rnn_train.py
@@ -32,14 +32,14 @@ USE_GLOVE = True  # Set to False to use random embeddings instead
 FREEZE_EMBEDDINGS = False  # Set to True to freeze GloVe embeddings during training
 
 BATCH_SIZE = 128
-MAX_VOCAB = 15000
+MAX_VOCAB = 10000
 MAX_LEN = 200
 # EMBED_DIM will be determined by GloVe file (e.g., 100 for glove.6B.100d.txt)
 RNN_UNITS = 128
 NUM_LAYERS = 2
 BIDIR = True
 RNN_TYPE = 'rnn'  # 'lstm', 'gru', or 'rnn'
-DROPOUT = 0.5
+DROPOUT = 0.3
 LR = 1e-3
 EPOCHS = 15
 
